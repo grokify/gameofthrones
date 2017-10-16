69747718b75be5beb17508b4bca26496ced6adf4
John Wang <johncwang@gmail.com>
2017-10-16 00:48:14 -0700

    add data converter example

---
+++ examples/build_data/build_data.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/grokify/gameofthrones"
+	"github.com/grokify/gotilla/fmt/fmtutil"
+	"github.com/grokify/gotilla/strconv/phonenumber"
+	"github.com/grokify/oauth2util-go/scimutil"
+)
+
+type Person struct {
+	AdditionalName string `json:"additionalName,omitempty"`
+	GivenName      string `json:"givenName,omitempty"`
+	FamilyName     string `json:"familyName,omitempty"`
+	DisplayName    string `json:"displayName,omitempty"`
+}
+
+func main() {
+	chars, err := gameofthrones.ReadCharacters()
+	if err != nil {
+		panic(err)
+	}
+
+	a2g := phonenumber.NewAreaCodeToGeo()
+	a2g.ReadData()
+	fng := phonenumber.NewFakeNumberGenerator(a2g.AreaCodes())
+
+	set := map[int]int{}
+	num := 0
+	for i, char := range chars {
+		num, set = fng.RandomLocalNumberUSUnique(set)
+		char.Character.PhoneNumbers = append(char.Character.PhoneNumbers,
+			scimutil.Item{Value: fmt.Sprintf("+%d", num)})
+		chars[i] = char
+	}
+	fmtutil.PrintJSON(chars)
+}

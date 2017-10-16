c4fbe51e39cf04d8174eb0861c2b8aac68183312
John Wang <johncwang@gmail.com>
2017-10-16 06:59:59 -0700

    add fictitious email

--- examples/build_data/build_data.go
+++ examples/build_data/build_data.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 
 	"github.com/grokify/gameofthrones"
 	"github.com/grokify/gotilla/fmt/fmtutil"
+	"github.com/grokify/gotilla/io/ioutilmore"
+	"github.com/grokify/gotilla/net/urlutil"
 	"github.com/grokify/gotilla/strconv/phonenumber"
 	"github.com/grokify/oauth2util-go/scimutil"
 )
@@ -16,7 +20,7 @@ type Person struct {
 	DisplayName    string `json:"displayName,omitempty"`
 }
 
-func main() {
+func buildBaseData() {
 	chars, err := gameofthrones.ReadCharacters()
 	if err != nil {
 		panic(err)
@@ -31,9 +35,42 @@ func main() {
 	num := 0
 	for i, char := range chars {
 		num, set = fng.RandomLocalNumberUSUnique(set)
-		char.Character.PhoneNumbers = append(char.Character.PhoneNumbers,
+		char.Character.PhoneNumbers = append(
+			char.Character.PhoneNumbers,
 			scimutil.Item{Value: fmt.Sprintf("+%d", num)})
+
 		chars[i] = char
 	}
 	fmtutil.PrintJSON(chars)
+}
+
+func addEmail(chars []gameofthrones.Character) []gameofthrones.Character {
+	for i, char := range chars {
+		charName := char.Character.DisplayName
+		charSlug := urlutil.ToSlugLowerString(charName)
+		email := fmt.Sprintf("%v@example.com", charSlug)
+		char.Character.Emails = append(
+			char.Character.Emails,
+			scimutil.Item{Value: email})
+		chars[i] = char
+	}
+	return chars
+}
+
+func main() {
+	file := "characters_out.json"
+	chars := []gameofthrones.Character{}
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(bytes, &chars)
+	if err != nil {
+		panic(err)
+	}
+	chars = addEmail(chars)
+	fmtutil.PrintJSON(chars)
+	outfile := "characters_out_email.json"
+	ioutilmore.WriteJSON(outfile, chars, 644, true)
+	//fmt.Println("DONE")
 }
